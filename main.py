36f264d5b005ff5f146c6380594fb36592a287ca
laote <ihagiu1212@gmail.com>
2024-10-10 14:02:03 +0300

    imported random,creating list, setting few function to replace the word with some random words from my list, setting the button to be functional when you press one will random import an word from the list,

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Import Modules
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
-
+from random import choice
 
 
 
@@ -27,6 +27,9 @@ text3 = QLabel("?")
 button1 = QPushButton("Press it")
 button2 = QPushButton("Press it")
 button3 = QPushButton("Press it")
+
+
+my_words = ['hello', 'goodbye', 'app', 'test', 'python', 'sql', 'css', 'django']
 
 
 
@@ -63,7 +66,34 @@ master_layout.addLayout(row3)
 
 main_window.setLayout(master_layout)
 
+
+
+# Functions Part
+
+def random_word1():
+    word = choice(my_words)
+    text1.setText(word)
+
+
+
+def random_word2():
+    word = choice(my_words)
+    text2.setText(word)
+
+
+
+def random_word3():
+    word = choice(my_words)
+    text3.setText(word)
+
+
 # Events
+
+button1.clicked.connect(random_word1)
+button2.clicked.connect(random_word2)
+button3.clicked.connect(random_word3)
+
+
 
 
 
