5d1de0bd9e5ee731b5316511d557468f6bf9a40b
laote <ihagiu1212@gmail.com>
2024-10-10 13:29:43 +0300

    the design part was implemented, buttons was added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Import Modules
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
 
 
 
@@ -16,12 +16,52 @@ main_window.resize(300, 200)
 
 # Create all App Objects
 
+title = QLabel("Something Temporary")
+
+
+text1 = QLabel("?")
+text2 = QLabel("?")
+text3 = QLabel("?")
+
+
+button1 = QPushButton("Press it")
+button2 = QPushButton("Press it")
+button3 = QPushButton("Press it")
+
+
 
 
 
 # Design Part 
 
+master_layout = QVBoxLayout()
 
+
+row1 = QHBoxLayout()
+row2 = QHBoxLayout()
+row3 = QHBoxLayout()
+
+
+row1.addWidget(title, alignment=Qt.AlignCenter)
+
+
+row2.addWidget(text1, alignment=Qt.AlignCenter)
+row2.addWidget(text2, alignment=Qt.AlignCenter)
+row2.addWidget(text3, alignment=Qt.AlignCenter)
+
+
+row3.addWidget(button1)
+row3.addWidget(button2)
+row3.addWidget(button3)
+
+
+
+master_layout.addLayout(row1)
+master_layout.addLayout(row2)
+master_layout.addLayout(row3)
+
+
+main_window.setLayout(master_layout)
 
 # Events
 
@@ -31,3 +71,4 @@ main_window.resize(300, 200)
 
 main_window.show()
 app.exec_()
+
