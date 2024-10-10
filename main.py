4325df2e0b43e0695da9496ec0b02add0beea96c
laote <ihagiu1212@gmail.com>
2024-10-10 12:59:04 +0300

    setting the workspace, making the imports and creating the window for the app

---
+++ main.py
@@ -0,0 +1,33 @@
+# Import Modules
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+
+
+
+
+# Main App Objects and Settings
+
+app = QApplication([])
+main_window = QWidget()
+main_window.setWindowTitle("Random word Maker")
+main_window.resize(300, 200)
+
+
+
+# Create all App Objects
+
+
+
+
+# Design Part 
+
+
+
+# Events
+
+
+
+# Show/Run the app
+
+main_window.show()
+app.exec_()
